49c7b5f34ab385a2cebc9b049e362d72d35d3e1f
aronmategubacsi <21.gubacsi.aron.mate@mechwart.com>
2023-11-09 12:52:08 +0100

    Add project files.

---
+++ typeConversion.cs
@@ -0,0 +1,10 @@
+﻿namespace Util
+{
+    class typeConversion : Base
+    {
+        public static int Int(string value) => int.Parse(value);
+        public static long Long(string value) => long.Parse(value);
+        public static float Float(string value) => float.Parse(value);
+        public static double Double(string value) => double.Parse(value);
+    }
+}
