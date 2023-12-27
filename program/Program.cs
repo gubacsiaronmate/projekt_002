d16b203565f2c850ea9e2e84eefaa6ed0a8875c6
aronmategubacsi <21.gubacsi.aron.mate@mechwart.com>
2023-12-27 21:26:32 +0100

    main menu inputs started

--- program/Program.cs
+++ program/Program.cs
@@ -1,5 +1,6 @@
 ﻿// ReSharper disable InconsistentNaming
 // ReSharper disable CheckNamespace
+using static Util.typeConversion;
 using static Util.Base;
 namespace Main;
 
@@ -7,32 +8,75 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        string headLine = "Welcome to our Computer Parts storage!";
+        // Variables:
+        string headLine = "Welcome to our Computer Parts warehouse!";
         string optText = "Please choose from this menu:";
         string[] options = { "[A]> Search Parts", "[B]> Add Part", "[C]> Print All", "[D]> Show Statistics", "[E]> Apply Discount", "[X]> Exit" };
+        string[] keys = {"A", "B", "C", "D", "E", "X"};
+        string howToChooseText = "Press the corresponding key to continue";
         int numOfSpaces = 3;
         string starLine = new string('*', (headLine.Length + (2 * numOfSpaces) + 2));
         string spaceLine = new string(' ', (headLine.Length + (2 * numOfSpaces)));
         string smallSpacer = new string(' ', numOfSpaces);
-        /* *   opt                     *
-                ||
-                V
-            *{smallSpacer}{opt}{' ' * (headLine.Length - opt.Length + numOfSpaces)}* 
-        */
+        
+        // Menu displayed:
         println(
             $"{starLine}"+
                 $"\n*{spaceLine}*"+
                 $"\n*{smallSpacer}{headLine}{smallSpacer}*"+
                 $"\n*{spaceLine}*"+
-                $"\n*{smallSpacer}{optText}{new string(' ', (headLine.Length - optText.Length + numOfSpaces))}*"+
-                $"\n*{spaceLine}*"
+                $"\n*{spaceLine}*"+
+                $"\n*{smallSpacer}{optText}{new string(' ', (headLine.Length - optText.Length + numOfSpaces))}*"
         );
         foreach (var opt in options)
         {
             println($"*{smallSpacer}{opt}{new string(' ', (headLine.Length - opt.Length + numOfSpaces))}*");
         }
-        println($"*{spaceLine}*\n{starLine}");
+        println(
+            $"*{spaceLine}*"+
+                $"\n*{smallSpacer}{howToChooseText}{new string(' ', (headLine.Length - howToChooseText.Length + numOfSpaces))}*"+
+                $"\n*{spaceLine}*"+
+                $"\n{starLine}\n"
+        );
+        
+        // Menu Input:
+        string pressedKey = getPressedKey().KeyChar.ToString().ToUpper();
+        
+        // Menu Input Handling:
+        while (Array.IndexOf(keys, pressedKey) == -1)
+        {
+            println(" <- This is an invalid input! Choose from the menu above!\n");
+            pressedKey = getPressedKey().KeyChar.ToString().ToUpper();
+        }
+        switch (pressedKey)
+        {
+            case "A":
+                println(String(pressedKey));
+                break;
+            
+            case "B":
+                println(String(pressedKey));
+                break;
+            
+            case "C":
+                println(String(pressedKey));
+                break;
+            
+            case "D":
+                println(String(pressedKey));
+                break;
+            
+            case "E":
+                println(String(pressedKey));
+                break;
+            
+            case "X":
+                println(String(pressedKey));
+                break;
+        }
     }
+
+    private static ConsoleKeyInfo getPressedKey() => Console.ReadKey();
 }
 
 /*
@@ -46,4 +90,9 @@ public static class Program
     *                                                *
     *                                                *
     **************************************************
- */
+
+
+    *   opt     * ==> *{smallSpacer}{opt}{' ' * (headLine.Length - opt.Length + numOfSpaces)}*
+
+*/
+ 
