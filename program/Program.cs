95a39735a041afe4fd6d702a24cc1dc5a95c271e
aronmategubacsi <21.gubacsi.aron.mate@mechwart.com>
2023-12-27 17:40:28 +0100

    main menu done

--- program/Program.cs
+++ program/Program.cs
@@ -1,11 +1,49 @@
 ﻿// ReSharper disable InconsistentNaming
 // ReSharper disable CheckNamespace
+using static Util.Base;
 namespace Main;
 
 public static class Program
 {
     public static void Main(string[] args)
     {
-        
+        string headLine = "Welcome to our Computer Parts storage!";
+        string optText = "Please choose from this menu:";
+        string[] options = { "[A]> Search Parts", "[B]> Add Part", "[C]> Print All", "[D]> Show Statistics", "[E]> Apply Discount", "[X]> Exit" };
+        int numOfSpaces = 3;
+        string starLine = new string('*', (headLine.Length + (2 * numOfSpaces) + 2));
+        string spaceLine = new string(' ', (headLine.Length + (2 * numOfSpaces)));
+        string smallSpacer = new string(' ', numOfSpaces);
+        /* *   opt                     *
+                ||
+                V
+            *{smallSpacer}{opt}{' ' * (headLine.Length - opt.Length + numOfSpaces)}* 
+        */
+        println(
+            $"{starLine}"+
+                $"\n*{spaceLine}*"+
+                $"\n*{smallSpacer}{headLine}{smallSpacer}*"+
+                $"\n*{spaceLine}*"+
+                $"\n*{smallSpacer}{optText}{new string(' ', (headLine.Length - optText.Length + numOfSpaces))}*"+
+                $"\n*{spaceLine}*"
+        );
+        foreach (var opt in options)
+        {
+            println($"*{smallSpacer}{opt}{new string(' ', (headLine.Length - opt.Length + numOfSpaces))}*");
+        }
+        println($"*{spaceLine}*\n{starLine}");
     }
 }
+
+/*
+ 
+    ************************************************** // 50
+    *                                                *
+    *                                                *
+    *                                                *
+    *                                                *
+    *                                                *
+    *                                                *
+    *                                                *
+    **************************************************
+ */
